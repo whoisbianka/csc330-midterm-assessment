4b57302315d88d552f23171c606128af64f96668
Bianka <151770472+whoisbianka@users.noreply.github.com>
2026-03-10 17:07:16 -0400

    edited app.py to just have return render template

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
- return "Flask app running on GCP"
+ return render_template('index.html')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+
 
 if __name__ == "__main__":
  app.run(debug=True, host="0.0.0.0",
