d03f64af205325059ee45e659aebbc34f113850f
Bianka <151770472+whoisbianka@users.noreply.github.com>
2026-03-11 12:18:11 -0400

    removed render template to try to get it to run

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
- return render_template('index.html')
-
-
+ return "Flask app running on GCP"
 
 if __name__ == "__main__":
  app.run(debug=True, host="0.0.0.0",
